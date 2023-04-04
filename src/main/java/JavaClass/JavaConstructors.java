68f7dfc1a118eeb3ff6d0522a691d27bbd408017
B-tag <behrooz_t@yahoo.com>
2023-04-04 17:25:23 +0100

    Constructors

---
+++ src/main/java/JavaClass/JavaConstructors.java
@@ -0,0 +1,20 @@
+package JavaClass;
+
+public class JavaConstructors {
+
+   public int age;
+   public String name;
+
+//    public JavaConstructors() {
+//    }  We can not have default constructor when we define another constructor
+
+    public JavaConstructors(int age) {
+        this.age = age;
+    }
+
+
+    public JavaConstructors(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/main/java/JavaClass/JavaMain.java
@@ -0,0 +1,19 @@
+package JavaClass;
+
+public class JavaMain {
+
+    public static void main(String[] args) {
+
+        JavaConstructors javaCons01 = new JavaConstructors(23);
+        javaCons01.name = "Tester";
+
+
+        JavaConstructors javaCons02 = new JavaConstructors("Tester");
+        javaCons02.age = 45;
+
+
+
+
+    }
+
+}
