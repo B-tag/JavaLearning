e30696d5cb9252853f786274285a1251c9dec795
B-tag <behrooz_t@yahoo.com>
2023-04-25 08:40:30 +0100

    Numeric value and Variables update

--- src/main/java/JavaVariables.java
+++ src/main/java/JavaVariables.java
@@ -4,25 +4,36 @@ public class JavaVariables {
     public static void main(String[] args) {
 
 
-        int a = 214;
+        int a01 = 214;
         int a02 = 213;
         short b = 55;
         long c = 8325;
         double d = 3.4567;
         float e = 5.235f;
         byte g = 127;
-        int i = a + b;
+        int i = a01 + b;
 //        System.out.println(i);
 //        System.out.println("Numbers: " + a + d); // not add numbers shows string
 
         boolean bool01 = true;
         boolean bool02 = false;
-        boolean bool03 = a < a02 ? bool01 : bool02;
+        boolean bool03 = a01 < a02 ? bool01 : bool02;
         System.out.println(bool03);
 //
 //
 //        String text = "pool";
 //        char chara = 'e';
 //        System.out.println(a + text);
+
+        String test = "TEST";
+        Character test02 = 'e';
+        System.out.println("Char test3 : " + test02);
+
+        char test3 = '3';
+        test02 = test3;
+
+        System.out.println("Char test3 : " + test3);
+        System.out.println("Char test3 : " + test02);
+
     }
 }
