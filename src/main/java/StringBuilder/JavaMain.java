3fe9a24eebe727d0514f8c54722e89ad78ddf293
B-tag <behrooz_t@yahoo.com>
2023-07-01 16:33:26 +0100

    String builder

--- src/main/java/StringBuilder/JavaMain.java
+++ src/main/java/StringBuilder/JavaMain.java
@@ -2,28 +2,34 @@ package StringBuilder;
 
 public class JavaMain {
     public static void main(String[] args) {
-        String name01="Hello";
+        String name01 = "Hello";
 
-        name01+= ", My ";
-        name01+="name is ";
-        name01+="Tester 01";
+        name01 += ", My ";
+        name01 += "name is ";
+        name01 += "Tester 01";
 
-        System.out.println(name01);
+        System.out.println(name01); // sys out 01
 
 
-        StringBuilder name02=new StringBuilder();
+        StringBuilder name02 = new StringBuilder();
 
         name02.append("My name");
         name02.append(" is tester");
-        name02.append("0"+2);
-        System.out.println(name02);
+        name02.append("0" + 2);
 
-        name02.deleteCharAt(5);
-        System.out.println(name02);
+        System.out.println("--------------------------------------------------");
+        System.out.println("After all appending the Length is : " + name02.length());
+        System.out.println(name02); // sys out 02 = My name is tester02
 
-        name02.insert(5,"---");
-        System.out.println(name02);
+        System.out.println("--------------------------------------------------");
+        name02.deleteCharAt(5); // Delete m
+        System.out.println("After Delete the Length is : " + name02.length());
+        System.out.println(name02); // sys out 03 = My nae is tester02
 
+        System.out.println("--------------------------------------------------");
+        name02.insert(5, "---"); // add "---" from index 5 then increase the length
+        System.out.println("After insertion the Length is : " + name02.length());
+        System.out.println(name02); // sys out 04 = My na---e is tester02
 
 
     }
