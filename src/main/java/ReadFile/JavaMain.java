710db87e01100ed2da1e45e85adeae8c5dc0bd66
B-tag <behrooz_t@yahoo.com>
2023-07-01 16:11:46 +0100

    File reader - Scanner and new scanner

--- src/main/java/ReadFile/JavaMain.java
+++ src/main/java/ReadFile/JavaMain.java
@@ -8,12 +8,24 @@ public class JavaMain {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        String path="E:\\Java\\JavaPractice\\new.txt";
+        String path = "E:\\Java\\JavaPractice\\new.txt";
+//         E:\Java\JavaPractice\new.txt
 
-        File file=new File(path);
-        Scanner scanner =new Scanner(file);
+        File file = new File(path);
+        Scanner scanner = new Scanner(file);
 
-        while (scanner.hasNextLine()){
+        System.out.println("The int value_01 : " + scanner.nextInt());
+        System.out.println("The int value_02 : " + scanner.nextInt());
+
+        while (scanner.hasNextLine()) {
+            String next = scanner.next();
+            System.out.println(next);
+        }
+
+        System.out.println("\n----------------");
+        System.out.println("\nNew read :");
+        scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
             String next = scanner.next();
             System.out.println(next);
         }
