7fdee8274f7461c9ae308553b84783fc0b492341
B-tag <behrooz_t@yahoo.com>
2023-07-01 17:54:05 +0100

    abstract class

--- src/main/java/JavaAbstractClass/JavaAbstractClass.java
+++ src/main/java/JavaAbstractClass/JavaAbstractClass.java
@@ -4,10 +4,20 @@ public class JavaAbstractClass {
 
     public static void main(String[] args) {
 
-        MyCarClass myCarClass=new MyCarClass();
+        MyCarClass myCarClass = new MyCarClass();
 
-        myCarClass.carName="Tesla";
-        myCarClass.serialNumber=123;
+        myCarClass.carName = "Tesla";
+        myCarClass.serialNumber = 123;
+
+        // only we can instantiate when we have an
+        // abstract method in Abstract class
+        AbstractClass abstractClass = new AbstractClass() {
+            @Override
+            public void test01() {
+                System.out.println("Test 01");
+            }
+        };
+        abstractClass.test01();
 
     }
 
