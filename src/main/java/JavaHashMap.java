57c0ccb882cd75f1f7b2baed0c77639c3826d579
Behrooz <53882107+B-tag@users.noreply.github.com>
2023-07-16 00:49:22 +0100

    interview Practice

--- src/main/java/JavaHashMap.java
+++ src/main/java/JavaHashMap.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -5,7 +7,12 @@ public class JavaHashMap {
 
     public static void main(String[] args) {
 
+        ArrayList<String> myArray = new ArrayList<>();
+        String name = "tedf;kgjhrtsoasponbbbeeest";
+
+
         HashMap<Integer, String> myHashMap = new HashMap<>();
+        HashMap<Integer, ArrayList<String>> otherHashMap = new HashMap<>();
 
         myHashMap.put(1, "ABC-01");
         myHashMap.put(2, "ABC-02");
@@ -24,6 +31,27 @@ public class JavaHashMap {
 
         System.out.println();
         extracted(myHashMap);
+
+        myArray.add("test");
+        myArray.add("stet");
+        myArray.add("ttes");
+        myArray.add("ttse");
+        myArray.add("estt");
+        myArray.add("sett");
+
+        // sort a String in this one "name"
+        name = name.chars()
+                .sorted()
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        System.out.println(name);
+
+        otherHashMap.put(1, myArray);
+
+        System.out.println(otherHashMap);
+
+
     }
 
     private static void extracted(HashMap<Integer, String> myMap) {
@@ -32,4 +60,18 @@ public class JavaHashMap {
             System.out.println("  --- Value :" + loopCounter.getValue());
         }
     }
+
+    // other way to sort a string
+    private static String sort(String text) {
+        char[] chars = prepare(text).toCharArray();
+
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+    private static String prepare(String text) {
+        return text.toLowerCase()
+                .trim()
+                .replaceAll("\\s+", "");
+    }
+
 }
