287fcf99f30a6bc5f0e44e62866840a6c5033288
B-tag <behrooz_t@yahoo.com>
2023-07-17 10:17:51 +0100

    Create TempJavaClass.java

---
+++ src/main/java/_PracticeTemp/TempJavaClass.java
@@ -0,0 +1,20 @@
+package _PracticeTemp;
+
+import java.util.ArrayList;
+
+public class TempJavaClass {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> myList=new ArrayList<>();
+
+        myList.add("A");
+        myList.add("B");
+        myList.add("C");
+        myList.add(1,"D");
+
+        System.out.println(myList);
+
+    }
+
+}
